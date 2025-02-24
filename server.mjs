46332477939f1e29a46d7331d665b9d1d6080a0d
Kayla Harris <103599117+k281harri@users.noreply.github.com>
2025-02-24 11:14:50 -0600

    corrected mongodb code

--- server.mjs
+++ server.mjs
@@ -20,9 +20,12 @@ app.use('/todo', todoRoutes);
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected')
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+
 
 // Listener
 app.listen(PORT, () => {
   console.log(`Server Running on PORT: ${PORT}`);
-}))
+})
