033be98b93df8ac97d573e44a5ae23fe278569c0
Kayla Harris <103599117+k281harri@users.noreply.github.com>
2025-02-23 21:59:29 -0600

    boilerplate creation

---
+++ cpp.jsx
@@ -0,0 +1,9 @@
+// This is a basic JavaScript boilerplate
+// Function to display a message
+function displayMessage() {
+    const message = "Welcome to JavaScript!";
+    document.getElementById("demo").innerText = message;
+}
+
+// Call the function to display the message
+displayMessage();
