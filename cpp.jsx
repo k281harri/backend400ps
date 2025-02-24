2615d438756009f5a19f8e6201193e1f46c9364b
Kayla Harris <103599117+k281harri@users.noreply.github.com>
2025-02-24 13:32:11 -0600

    deleted extra files

--- cpp.jsx
+++
@@ -1,9 +0,0 @@
-// This is a basic JavaScript boilerplate
-// Function to display a message
-function displayMessage() {
-    const message = "Welcome to JavaScript!";
-    document.getElementById("demo").innerText = message;
-}
-
-// Call the function to display the message
-displayMessage();
--- server.mjs
+++ server.mjs
@@ -3,25 +3,28 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.mjs';
 import todoRoutes from './routes/todoRoutes.mjs';
+import cors from 'cors';
 
 // Setup
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.port || 3000;
 
 // DB Connection
 connectDB();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 // Routes
 app.use('/todo', todoRoutes);
 
-// MongoDB connection
-mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+// // MongoDB connection
+// mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.error('MongoDB connection error:', err));
 
 
 
